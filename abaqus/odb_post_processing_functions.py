70cb17c183620184c4c3cdff7a05015a2e5c99d6
erolsson <erolsson@kth.se>
2018-05-08 10:47:47 +0200

    Frist version

--- abaqus/odb_post_processing_functions.py
+++ abaqus/odb_post_processing_functions.py
@@ -41,5 +41,5 @@ def get_node_data_from_set(odb, node_set_name):
     return node_dict
 
 if __name__ == '__main__':
-    odb = odbAccess.openOdb('scratch/users/erik/Abaqus/Gear/planetaryGear/odb/danteTooth201702200')
+    odb = odbAccess.openOdb('/scratch/users/erik/Abaqus/Gear/planetaryGear/odb/danteTooth201702200')
     odb.close()
