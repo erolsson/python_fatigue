2e46080295110d086926deb7c2a20b8f4b90df67
erolsson <erolsson@kth.se>
2018-05-23 14:57:38 +0200

    Frist version

--- planetary_gear/carbon/write_carbon_along_paths.py
+++ planetary_gear/carbon/write_carbon_along_paths.py
@@ -1,11 +1,11 @@
 import sys
 import os
 curr_path = os.getcwd()
-sys.path.append(curr_path[:-len('/planetary_gear/carbon')])
-sys.path.append(sys.path[-1]+'/abaqus')
+sys.path.append(curr_path[:-len(r'/planetary_gear/carbon')])
+sys.path.append(sys.path[-1]+'r/abaqus')
 print sys.path
 print "Current path:", curr_path
-print "Main path:", curr_path[:-len('/planetary_gear/carbon')]
+print "Main path:", curr_path[:-len(r'/planetary_gear/carbon')]
 
 import numpy as np
 import pickle 
