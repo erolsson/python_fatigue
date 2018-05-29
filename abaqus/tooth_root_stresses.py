8b6bc46f4dc283d35ffab0b62f21b2faeeee03c6
erolsson <erolsson@kth.se>
2018-05-29 10:39:57 +0200

    Frist version

--- abaqus/tooth_root_stresses.py
+++ abaqus/tooth_root_stresses.py
@@ -66,5 +66,5 @@ if __name__ == '__main__':
                                        includeIntersections=False)
 
             print xy
-        odb.close()
+        # odb.close()
     print root_data
