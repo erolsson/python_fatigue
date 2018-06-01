96bcf257d1cb3881a8c9121eb9a9f79197b7ec32
erolsson <erolsson@kth.se>
2018-06-01 16:51:20 +0200

    Frist version

--- planetary_gear/rotate_tooth_mesh.py
+++ planetary_gear/rotate_tooth_mesh.py
@@ -1,3 +1,4 @@
+from math import pi
 import numpy as np
 
 from write_input_for_dante import read_nodes_and_elements
@@ -9,7 +10,7 @@ nodes, elements = read_nodes_and_elements('input_files/gear_models/planet_gear/m
 
 point = nodes[nodes[:, 0] == center_node, 1:3][0]
 
-q = -np.arctan(point[0]/point[1])
+q = -np.arctan(point[0]/point[1]) + pi/2
 rot = np.array([[np.cos(q), -np.sin(q), 0],
                 [np.sin(q), np.cos(q), 0],
                 [0, 0, 1]])
@@ -17,4 +18,5 @@ rot = np.array([[np.cos(q), -np.sin(q), 0],
 for n in nodes:
     n[1:] = np.dot(n[1:], rot)
 
+nodes[np.abs(nodes[:, 1]) < 1e-9, 1] = 0.
 write_geom_include_file(nodes, elements, 'input_files/gear_models/planet_gear/coarse_mesh.inc')
