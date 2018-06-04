20477ef4582d6e98beab4d67ed578bd0e6fe21b2
erolsson <erolsson@kth.se>
2018-06-04 16:26:19 +0200

    Frist version

--- planetary_gear/write_pulsator_model.py
+++ planetary_gear/write_pulsator_model.py
@@ -231,11 +231,13 @@ if __name__ == '__main__':
     file_lines += write_load_step('Initiate_contact')
     for load_amp in load_amplitudes:
         mean_load = (1 + load_ratio)/(1-load_ratio)*load_amp
-        file_lines += write_load_step('Amplitude_Pamp=' + str(load_amp) + 'kN_min', force=(mean_load-load_amp)*1000)
+        file_lines += write_load_step('Amplitude_Pamp=' + str(load_amp).replace('.', '_') + 'kN_min',
+                                      force=(mean_load-load_amp)*1000)
 
     for load_amp in load_amplitudes:
         mean_load = (1 + load_ratio)/(1-load_ratio)*load_amp
-        file_lines += write_load_step('Amplitude_Pamp=' + str(load_amp) + 'kN_max', force=(mean_load+load_amp)*1000)
+        file_lines += write_load_step('Amplitude_Pamp=' + str(load_amp).replace('.', '_') + 'kN_max',
+                                      force=(mean_load+load_amp)*1000)
 
     with open('input_files/pulsator_model/pulsator_simulation.inp', 'w') as input_file:
         for line in file_lines:
