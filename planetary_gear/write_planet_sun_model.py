b21853ff82123008bc64be11472c0d37b5b8829d
erolsson <erolsson@kth.se>
2018-06-07 14:10:27 +0200

    Frist version

--- planetary_gear/write_planet_sun_model.py
+++ planetary_gear/write_planet_sun_model.py
@@ -5,31 +5,48 @@ from write_pulsator_model import write_include_files_for_tooth
 
 from write_pulsator_model import write_tooth_part
 
+
+def write_load_step(step_name, planet_torque=None, initial_inc=0.01):
+    lines = ['*step, name=' + step_name + ', nlgeom=Yes',
+             '\t*Static',
+             '\t\t' + str(initial_inc) + ', 1., 1e-12, 1.']
+
+    if planet_torque:
+        pass
+    lines.append('\t*Output, field')
+    lines.append('\t\t*Element Output')
+    lines.append('\t\t\tS')
+    lines.append('\t\t*Node Output')
+    lines.append('\t\t\tU')
+    lines.append('*End step')
+    return lines
+
+
 if __name__ == '__main__':
     gear_model_dir = 'input_files/gear_models/'
     simulation_dir = 'input_files/planet_sun/'
 
     torque = 1500.
 
-    Gear = namedtuple('Gear', ['name', 'number_of_teeth', 'teeth_to_model', 'teeth_array', 'position', 'rotation'])
+    Gear = namedtuple('Gear', ['number_of_teeth', 'teeth_to_model', 'teeth_array', 'position', 'rotation'])
+    gears = {'planet': Gear(number_of_teeth=20, teeth_to_model=5, teeth_array=[], position=(0., 83.5, 0.),
+                            rotation=188),
+             'sun': Gear(number_of_teeth=24, teeth_to_model=5, teeth_array=[], position=(0., 0., 0.), rotation=0)}
 
-    gears = [Gear(name='planet', number_of_teeth=20, teeth_to_model=5, teeth_array=[], position=(0., 83.5, 0.),
-                  rotation=185),
-             Gear(name='sun', number_of_teeth=24, teeth_to_model=5, teeth_array=[], position=(0., 0., 0.), rotation=0)]
-
-    for gear, direction in zip(gears, [-1, 1]):
+    for (name, gear), direction in zip(gears.iteritems(), [-1, 1]):
         for i in range(gear.teeth_to_model):
-            gear.teeth_array.append(GearTooth(instance_name=gear.name + '_tooth' + str(i),
+            gear.teeth_array.append(GearTooth(instance_name=name + '_tooth' + str(i),
                                               rotation=(i+0.5)*360/gear.number_of_teeth*direction + gear.rotation,
-                                              part_names=[gear.name + '_coarse_tooth_pos',
-                                                          gear.name + '_coarse_tooth_neg'],
+                                              part_names=[name + '_coarse_tooth_pos',
+                                                          name + '_coarse_tooth_neg'],
                                               position=gear.position))
 
     # Tooth number 1 is the interesting tooth for fatigue, give it a denser mesh and a different name
-    gears[0].teeth_array[2].instance_name = 'eval_tooth'
-    gears[0].teeth_array[1].part_names = ['planet_dense_tooth_pos', 'planet_coarse_tooth_neg']
-    gears[0].teeth_array[2].part_names = ['planet_dense_tooth_pos', 'planet_dense_tooth_neg']
-    gears[0].teeth_array[3].part_names = ['planet_coarse_tooth_pos', 'planet_dense_tooth_neg']
+    planet = gears['planet']
+    planet.teeth_array[2].instance_name = 'eval_tooth'
+    planet.teeth_array[1].part_names = ['planet_dense_tooth_pos', 'planet_coarse_tooth_neg']
+    planet.teeth_array[2].part_names = ['planet_dense_tooth_pos', 'planet_dense_tooth_neg']
+    planet.teeth_array[3].part_names = ['planet_coarse_tooth_pos', 'planet_dense_tooth_neg']
 
     for gear, mesh in zip(['sun', 'planet', 'planet'], ['coarse', 'coarse', 'dense']):
         mesh_dir = gear_model_dir + gear + '_gear/'
@@ -52,10 +69,26 @@ if __name__ == '__main__':
     file_lines.append('\t\t200E3, 0.3')
     file_lines.append('*Assembly, name=pulsator_model')
 
-    for gear in gears:
+    for gear in gears.itervalues():
         for tooth in gear.teeth_array:
             file_lines += tooth.write_input()
 
+    # Combining surfaces to master contact and slave surfaces
+    # Sun gear master due to coarser mesh
+    for name, gear in gears.iteritems():
+        exposed_surface_names = [tooth.instance_name + '_' + str(i) + '.Exposed_Surface'
+                                 for tooth in gear.teeth_array for i in [0, 1]]
+
+        file_lines.append('\t*Surface, name=contact_Surface_' + name + ', combine=union')
+        for surface_name in exposed_surface_names:
+            file_lines.append('\t\t' + surface_name)
+        # Fixing surfaces for applying boundary conditions
+        file_lines.append('\t*Surface, name=bc_Surface_' + name + ', combine=union')
+        file_lines.append('\t\t' + gear.teeth_array[0].instance_name + '_0.x1_Surface')
+        file_lines.append('\t\t' + gear.teeth_array[-1].instance_name + '_1.x1_Surface')
+
+    file_lines.append('*End Assembly')
+    file_lines += write_load_step('Dummy')
     with open(simulation_dir + 'planet_sun_' + str(int(torque)) + '_Nm.inp', 'w') as input_file:
         for line in file_lines:
             input_file.write(line + '\n')
