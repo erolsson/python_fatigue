88030331dd62fb33325568efca4d151cf9679668
erolsson <erolsson@kth.se>
2018-06-19 22:41:52 +0200

    Frist version

--- planetary_gear/write_planet_ring_model.py
+++ planetary_gear/write_planet_ring_model.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
 
     Gear = namedtuple('Gear', ['number_of_teeth', 'teeth_to_model', 'teeth_array', 'position', 'rotation'])
     gears = {'planet': Gear(number_of_teeth=20, teeth_to_model=5, teeth_array=[], position=(0., 83.5, 0.),
-                            rotation=-360./22*5-12),
+                            rotation=-360./22*5-15),
              'ring': Gear(number_of_teeth=66, teeth_to_model=5, teeth_array=[], position=(0., 0., 0.),
                           rotation=-360./66*5)}
 
