ae74137966ad28d0ab855fc9a00c291bab174a2c
erolsson <erolsson@kth.se>
2018-06-28 10:28:20 +0200

    Frist version

--- abaqus/create_odb.py
+++ abaqus/create_odb.py
@@ -1,3 +1,5 @@
+from collections import namedtuple
+
 from odbAccess import *
 from abaqusConstants import *
 
@@ -5,10 +7,17 @@ import os
 
 from input_file_reader.input_file_functions import read_nodes_and_elements
 
+OdbInstance = namedtuple('OdbInstance', ['name', 'nodes', 'elements'])
 
-def create_odb(odb_file_name, model_data):
+
+def create_odb(odb_file_name, instance_data):
     print os.path.basename(odb_file_name), os.path.dirname(odb_file_name)
     odb = Odb(name=os.path.basename(odb_file_name), path=odb_file_name)
+
+    for instance in instance_data:
+
+        part = odb.Part(name=instance.name, embeddedSpace=THREE_D, type=DEFORMABLE_BODY)  # Todo Implement 2D models
+        odb.rootAssembly.Instance(name=instance.name, object=part)
 
     odb.update()
     odb.save()
@@ -18,6 +27,8 @@ if __name__ == '__main__':
     input_file_name = '/scratch/users/erik/python_fatigue/planetary_gear/' \
                       'input_files/planet_sun/planet_dense_geom_xpos.inc'
     nodes_pos, elements_pos = read_nodes_and_elements(input_file_name)
+    print nodes_pos, elements_pos
     model_data = 0
+    instances = [OdbInstance(name='right_part', nodes=nodes_pos, elements=elements_pos)]
     create_odb(odb_file_name=r'/scratch/users/erik/scania_gear_analysis/odb_files/stress.odb',
-               model_data=model_data)
+               instance_data=instances)
