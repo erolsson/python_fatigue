669f05a3c5ed4989a507f41e6a764a74e0da5de6
erolsson <erolsson@kth.se>
2018-06-29 14:51:06 +0200

    Frist version

--- abaqus/transfer_planet_stresses.py
+++ abaqus/transfer_planet_stresses.py
@@ -33,7 +33,7 @@ def transfer_gear_stresses(from_odb_name, to_odb_name):
     write_odb.close()
 
     for frame in frames:
-        process = Popen(['abaqus viewer noGUI=_copy_planet_stress.py'], cwd=os.getcwd())
+        process = Popen(['abaqus', 'viewer', 'noGUI=_copy_planet_stress.py'], cwd=os.getcwd())
         #  process = Popen(['abaqus viewer noGUI=_copy_planet_stress.py', from_odb_name,  to_odb_name,
         #                 frame.step, str(frame.number), str(frame_counter)])
         process.wait()
