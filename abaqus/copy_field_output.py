04556a34e8cf925c2f1ccc09e2434e2ca3f9cf60
erolsson <erolsson@kth.se>
2018-07-04 14:53:40 +0200

    Frist version

--- abaqus/copy_field_output.py
+++ abaqus/copy_field_output.py
@@ -10,7 +10,7 @@ step = odb.steps['Carburization-3']
 frame = step.frames[len(step.frames) - 1]
 field = frame.fieldOutputs['CONC']
 new_step = odb.Step(name='dante_data', description='', domain=TIME, timePeriod=1)
-new_frame = step.Frame(incrementNumber=0, frameValue=0, description='')
+new_frame = new_step.Frame(incrementNumber=0, frameValue=0, description='')
 new_field = -2.07934384e+04*field*field + 2.98225944e+02*field-1.60574051e-01
 new_frame.FieldOutput(name='NT', field=new_field)
 
