dfb7c5cffd3639ca48aa6e1f7dbd4dea34b4f2f7
erolsson <erolsson@kth.se>
2018-07-04 13:42:17 +0200

    Frist version

--- abaqus/copy_field_output.py
+++ abaqus/copy_field_output.py
@@ -3,7 +3,11 @@ import numpy as np
 
 from abaqusConstants import *
 
-odb = odbAccess.openOdb('/scratch/users/erik/scania_gear_analysis/odb_files/dante_interpolation/danteTooth20170220.odb')
+odb = odbAccess.openOdb('/scratch/users/erik/scania_gear_analysis/odb_files/dante_interpolation/danteTooth20170220.odb',
+                        readOnly=False)
 
 frame = odb.steps['danteResults_DC1_4'].frames[0]
 field = frame.fieldOutputs['Q_MARTENSITE']
+field = field.getSubset(position=NODAL)
+print field.values[0]
+frame.FieldOutput(name='NT', type=SCALAR)
