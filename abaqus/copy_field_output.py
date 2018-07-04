1e45311dcd0810678b0485933f53b8889ba6c544
erolsson <erolsson@kth.se>
2018-07-04 17:40:11 +0200

    Frist version

--- abaqus/copy_field_output.py
+++ abaqus/copy_field_output.py
@@ -12,7 +12,7 @@ field = frame.fieldOutputs['CONC']
 new_step = odb.Step(name='dante_data', description='', domain=TIME, timePeriod=1)
 new_frame = new_step.Frame(incrementNumber=0, frameValue=0, description='')
 new_field = -2.07934384e+04*field*field + 2.98225944e+02*field-1.60574051e-01
-new_frame.FieldOutput(name='NT', field=new_field)
+new_frame.FieldOutput(name='NT11', field=new_field)
 
 odb.save()
 odb.close()
