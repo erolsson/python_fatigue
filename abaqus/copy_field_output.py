d609abca741712712db5a9ec6de3ee571f587390
erolsson <erolsson@kth.se>
2018-07-04 17:52:54 +0200

    Frist version

--- abaqus/copy_field_output.py
+++ abaqus/copy_field_output.py
@@ -8,7 +8,7 @@ odb = odbAccess.openOdb('/scratch/users/erik/scania_gear_analysis/odb_files/dant
 
 step = odb.steps['Carburization-3']
 frame = step.frames[len(step.frames) - 1]
-field = frame.fieldOutputs['CONC']
+field = frame.fieldOutputs['NNC11']
 new_step = odb.Step(name='dante_data', description='', domain=TIME, timePeriod=1)
 new_frame = new_step.Frame(incrementNumber=0, frameValue=0, description='')
 new_field = -2.07934384e+04*field*field + 2.98225944e+02*field-1.60574051e-01
