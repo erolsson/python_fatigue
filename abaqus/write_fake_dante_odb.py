7fb747b221ca3b0b0cfadc84bd2d7a8e4b123935
erolsson <erolsson@kth.se>
2018-07-06 15:37:53 +0200

    Frist version

---
+++ abaqus/write_fake_dante_odb.py
@@ -0,0 +1,51 @@
+from odbAccess import *
+from abaqusConstants import *
+
+from input_file_reader.input_file_functions import read_nodes_and_elements
+
+from create_odb import create_odb
+from create_odb import OdbInstance
+
+from planetary_gear.carbon.heat_treatment_correlation import carbon_to_hardness
+
+
+def create_dante_step(results_odb_name, carbon_odb_name, stress_odb_name, results_step_name):
+    carbon_odb = openOdb(carbon_odb_name, readOnly=True)
+    step = carbon_odb.steps['Carburization-3']
+    frame = step.frames[len(step.frames) - 1]
+    field = frame.fieldOutputs['NNC11']
+
+    hardness = carbon_to_hardness(field)
+    results_odb = openOdb(results_odb_name, readOnly=False)
+    new_step = results_odb.Step(name=results_step_name, description='', domain=TIME, timePeriod=1)
+    new_frame = new_step.Frame(incrementNumber=0, frameValue=0, description='')
+
+    new_frame.FieldOutput(name='HV', field=hardness)
+    carbon_odb.close()
+
+    stress_odb = openOdb(stress_odb_name, readOnly=True)
+    step = carbon_odb.steps['Equilibrium']
+    frame = step.frames[len(step.frames) - 1]
+    field = frame.fieldOutputs['S']
+    new_frame.FieldOutput(name='S', field=field)
+    stress_odb.close()
+
+    results_odb.close()
+
+if __name__ == '__main__':
+    carbon_odb_path = '/scratch/users/erik/python_fatigue/planetary_gear/input_files/resolve_residual_stresses/'
+    dante_odb_path = '/scratch/users/erik/scania_gear_analysis/odb_files/'
+
+    input_file_name = '/scratch/users/erik/python_fatigue/planetary_gear/' \
+                      'input_files/planet_sun/planet_dense_geom_xpos.inc'
+    nodes_pos, elements_pos = read_nodes_and_elements(input_file_name)
+    instances = [OdbInstance(name='tooth_right', nodes=nodes_pos, elements=elements_pos)]
+
+    tooth_odb_file_name = dante_odb_path + 'dante_results_fake.odb'
+    create_odb(odb_file_name=tooth_odb_file_name, instance_data=instances)
+
+    for cd in ['0_5', '0_8', '1_1', '1_4']:
+        create_dante_step(results_odb_name=tooth_odb_file_name,
+                          carbon_odb_name=carbon_odb_path + 'Toolbox_Carbon_' + cd + '_quarter.odb',
+                          stress_odb_name=dante_odb_path + 'residual_stresses_' + cd + '.odb',
+                          results_step_name='dante_results_' + cd)
--- planetary_gear/carbon/heat_treatment_correlation.py
+++ planetary_gear/carbon/heat_treatment_correlation.py
@@ -7,7 +7,7 @@ case_depths = [0.8]
 color = ['r', 'b', 'g', 'k']
 
 
-def martensite(carbon):
+def carbon_to_martensite(carbon):
     def poly(x):
         return -2.07934384e+04*x**2 + 2.98225944e+02*x - 1.60574051e-01
     if np.isscalar(carbon):
@@ -15,6 +15,10 @@ def martensite(carbon):
     q = poly(carbon)
     q[carbon < 0.0022] = poly(0.0022)
     return q
+
+
+def carbon_to_hardness(carbon):
+    return -1.95704040e+09*carbon**3 + 1.79113930e+07*carbon**2 + 5.50685403e+04*carbon + 2.27359677e+02
 
 for case_depth, c in zip(case_depths, color):
     with open('../pickles/tooth_root_data/data' + str(case_depth).replace('.', '_') + '.pkl') as pickle_handle:
@@ -29,10 +33,17 @@ for case_depth, c in zip(case_depths, color):
     print np.polyfit(x, y, 2)
 
     x = np.linspace(0.002, 0.01, 1000)
-    plt.plot(x, martensite(x))
+    plt.plot(x, carbon_to_martensite(x))
 
     plt.figure(1)
     plt.plot(flank_data[:, 1], flank_data[:, 2], c, lw=2)
     plt.plot(root_data[:, 1], root_data[:, 2], '--' + c, lw=2)
+
+    x = root_data[root_data[:, 1] > 0.0023, 1]
+    y = root_data[root_data[:, 1] > 0.0023, 2]
+    print np.polyfit(x, y, 3)
+
+    x = np.linspace(0.002, 0.01, 1000)
+    plt.plot(x, carbon_to_hardness(x))
 
 plt.show()
