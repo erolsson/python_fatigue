d189fa5b396c3fa4ac021b59c5a25ed2c4fcfd5c
erolsson <erolsson@kth.se>
2018-08-15 11:27:33 +0200

    Frist version

--- abaqus/create_dante_odb.py
+++ abaqus/create_dante_odb.py
@@ -19,23 +19,24 @@ def create_dante_step(odb_name, pickle_directory, results_step_name):
     for pickle_file in pickle_files:
         prefix = pickle_file[len('Toolbox_Mechanical_0_5_quarter_'):]
         prefix = prefix[:-4]
-        print prefix
-    """
-    write_field_to_odb(field_data=stress, field_id='S', odb_file_name=odb_name, step_name=results_step_name,
-                       instance_name='tooth_right', frame_number=0,
-                       invariants=[MISES, MAX_PRINCIPAL, MID_PRINCIPAL, MIN_PRINCIPAL])
+        with open(pickle_directory + pickle_file, 'r') as pickle_handle:
+            pickle.load(pickle_handle)
+            pickle.load(pickle_handle)
+            data_dict[prefix] = pickle.load(pickle_handle)
 
-    stress = flip_node_order(stress, axis='z')
-    stress[:, 3] *= -1
-    write_field_to_odb(field_data=stress, field_id='S', odb_file_name=odb_name, step_name=results_step_name,
-                       instance_name='tooth_left', frame_number=0,
-                       invariants=[MISES, MAX_PRINCIPAL, MID_PRINCIPAL, MIN_PRINCIPAL])
-    """
+    scalar_fields = data_dict.keys()
+    scalar_fields.remove('S')
+
+    for scalar_field in scalar_fields:
+        write_field_to_odb(field_data=data_dict[scalar_field], field_id=scalar_field, odb_file_name=odb_name,
+                           step_name=results_step_name, instance_name='tooth_right', frame_number=0)
+        write_field_to_odb(field_data=data_dict[scalar_field], field_id=scalar_field, odb_file_name=odb_name,
+                           step_name=results_step_name, instance_name='tooth_left', frame_number=0)
 
 
 if __name__ == '__main__':
     dante_odb_path = '/scratch/users/erik/scania_gear_analysis/odb_files/heat_treatment/mesh_1x/'
-    pickle_directory = '/scratch/users/erik/scania_gear_analysis/pickles/heat_treatment/mesh_1x/fem_results/'
+    base_pickle_directory = '/scratch/users/erik/scania_gear_analysis/pickles/heat_treatment/mesh_1x/fem_results/'
 
     input_file_name = '/scratch/users/erik/python_fatigue/planetary_gear/' \
                       'input_files/planet_sun/planet_dense_geom_xpos.inc'
@@ -52,5 +53,5 @@ if __name__ == '__main__':
 
     for cd in [0.5, 0.8, 1.1, 1.4]:
         create_dante_step(odb_name=odb_file_name,
-                          pickle_directory=pickle_directory + 'case_depth_' + str(cd).replace('.', '_'),
+                          pickle_directory=base_pickle_directory + 'case_depth_' + str(cd).replace('.', '_'),
                           results_step_name='dante_results_' + str(cd).replace('.', '_'))
