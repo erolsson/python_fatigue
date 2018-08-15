5beb7e20273d86c57ef76ab8f5b491421cbc0632
erolsson <erolsson@kth.se>
2018-08-15 11:15:41 +0200

    Frist version

--- abaqus/create_dante_odb.py
+++ abaqus/create_dante_odb.py
@@ -1,6 +1,7 @@
 from odbAccess import *
 from abaqusConstants import MISES, MAX_PRINCIPAL, MID_PRINCIPAL, MIN_PRINCIPAL
 
+import os
 import pickle
 
 from input_file_reader.input_file_functions import read_nodes_and_elements
@@ -11,10 +12,11 @@ from odb_io_functions import write_field_to_odb
 from odb_io_functions import cylindrical_system_z
 from odb_io_functions import flip_node_order
 
-"""
-def create_dante_step(odb_name, results_step_name):
 
-
+def create_dante_step(odb_name, pickle_directory, results_step_name):
+    files = os.listdir(pickle_directory)
+    print files
+    """
     write_field_to_odb(field_data=stress, field_id='S', odb_file_name=odb_name, step_name=results_step_name,
                        instance_name='tooth_right', frame_number=0,
                        invariants=[MISES, MAX_PRINCIPAL, MID_PRINCIPAL, MIN_PRINCIPAL])
@@ -24,8 +26,8 @@ def create_dante_step(odb_name, results_step_name):
     write_field_to_odb(field_data=stress, field_id='S', odb_file_name=odb_name, step_name=results_step_name,
                        instance_name='tooth_left', frame_number=0,
                        invariants=[MISES, MAX_PRINCIPAL, MID_PRINCIPAL, MIN_PRINCIPAL])
-    
-"""
+    """
+
 
 if __name__ == '__main__':
     dante_odb_path = '/scratch/users/erik/scania_gear_analysis/odb_files/heat_treatment/mesh_1x/'
@@ -41,8 +43,10 @@ if __name__ == '__main__':
     instances = [OdbInstance(name='tooth_right', nodes=nodes_pos, elements=elements_pos),
                  OdbInstance(name='tooth_left', nodes=nodes_neg, elements=elements_neg)]
 
-    create_odb(odb_file_name=dante_odb_path + 'dante_results.odb', instance_data=instances)
-    pickle_file = pickle_directory + 'case_depth_0_5/Toolbox_Mechanical_0_5_quarter_S.pkl'
-    with open(pickle_file, 'r') as pickle_handle:
-        data = pickle.load(pickle_handle)
-        print data
+    odb_file_name = dante_odb_path + 'dante_results.odb'
+    create_odb(odb_file_name=odb_file_name, instance_data=instances)
+
+    for cd in [0.5, 0.8, 1.1, 1.4]:
+        create_dante_step(odb_name=odb_file_name,
+                          pickle_directory=pickle_directory + 'case_depth_' + str(cd).replace('.', '.'),
+                          results_step_name='dante_results_' + str(cd).replace('.', '_'))
