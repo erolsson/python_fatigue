9c9e87f5bc95fa04aef3137b1f043535897c6c07
erolsson <erolsson@kth.se>
2018-08-17 17:08:58 +0200

    Frist version

--- abaqus/write_dante_evaluation_pickles.py
+++ abaqus/write_dante_evaluation_pickles.py
@@ -4,12 +4,16 @@ from abaqusConstants import MISES, MAX_PRINCIPAL, MID_PRINCIPAL, MIN_PRINCIPAL
 import os
 import pickle
 
+import numpy as np
+
 
 def write_pickle_for_case_depth(odb_file_name, pickle_file_name, fatigue_set_name, fatigue_set_data):
     odb = openOdb(odb_file_name)
-    if fatigue_set_name not in odb.rootAssembly.elementSets:
-        odb.rootAssembly.instances['tooth_left'].ElementSetFromElementLabels(name=fatigue_set_name,
-                                                                             element_labels=fatigue_set_data.tolist())
+    for instance_name in ['left', 'right']:
+        if fatigue_set_name not in odb.rootAssembly.elementSets:
+            instance = odb.rootAssembly.instances['tooth_' + instance_name]
+            instance.ElementSetFromElementLabels(name=fatigue_set_name,
+                                                 element_labels=fatigue_set_data)
     odb.close()
 
 
@@ -17,4 +21,7 @@ if __name__ == '__main__':
     dante_odb_filename = '/scratch/users/erik/scania_gear_analysis/odb_files/heat_treatment/mesh_1x/dante_results.odb'
     pickle_directory = '/scratch/users/erik/scania_gear_analysis/pickles/heat_treatment/mesh_1x/'
 
+    set_file = '../planetary_gear/input_files/gear_models/planet_gear/tooth_root_set_volume_dense_mesh'
+    fatigue_set_data = np.genfromtxt(set_file, delimiter=',').tolist()
+    print fatigue_set_data
 
