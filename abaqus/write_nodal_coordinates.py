07db755ae0fdfe28095c83ef76670f504b9fa4c2
erolsson <erolsson@kth.se>
2018-08-17 18:36:06 +0200

    Frist version

--- abaqus/write_nodal_coordinates.py
+++ abaqus/write_nodal_coordinates.py
@@ -32,7 +32,7 @@ if __name__ == '__main__':
                                             element_set_name + '.inc')
 
     add_node_set(odb_file_name, node_set_name, node_labels, 'tooth_right')
-    add_element_set(odb_file_name, element_set_name, node_labels, 'tooth_right')
+    add_element_set(odb_file_name, element_set_name, element_labels, 'tooth_right')
 
     _, node_labels, _ = read_field_from_odb('HV', odb_file_name, element_set_name, step_name='dante_results_0_5',
                                             frame_number=0, instance_name='tooth_right', get_position_numbers=True)
