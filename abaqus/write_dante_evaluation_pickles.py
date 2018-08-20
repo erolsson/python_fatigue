6d75a3b078eca023cf8a1723bfefe0916f3b3295
erolsson <erolsson@kth.se>
2018-08-20 13:43:51 +0200

    Frist version

--- abaqus/write_dante_evaluation_pickles.py
+++ abaqus/write_dante_evaluation_pickles.py
@@ -1,10 +1,7 @@
 from odbAccess import *
 from abaqusConstants import MISES, MAX_PRINCIPAL, MID_PRINCIPAL, MIN_PRINCIPAL
 
-import os
 import pickle
-
-import numpy as np
 
 from materials.gear_materials import SteelData
 
@@ -20,12 +17,15 @@ def write_pickle_for_case_depth(odb_file_name, case_depth, pickle_file_name, fat
     dante_dict = {}
     step_name = 'dante_results_' + str(case_depth).replace('.', '_')
     for var in field_vars:
-        dante_dict[var] = read_field_from_odb(var, odb_file_name, element_set_name, step_name, frame_number=0,
+        dante_dict[var] = read_field_from_odb(var, odb_file_name, fatigue_set_name, step_name, frame_number=0,
                                               instance_name='tooth_left')
     steel_data = SteelData(**dante_dict)
-    residual_stress = read_field_from_odb('S', odb_file_name, element_set_name, step_name, frame_number=0,
+    residual_stress = read_field_from_odb('S', odb_file_name, fatigue_set_name, step_name, frame_number=0,
                                           instance_name='tooth_left', coordinate_system=cylindrical_system_z)
-    print np.max(residual_stress)
+
+    data_dict = {'steel_data': steel_data, 'S': residual_stress}
+    with open(pickle_file_name, 'w') as pickle_handle:
+        pickle.dump(data_dict, pickle_handle)
 
 
 if __name__ == '__main__':
@@ -34,11 +34,13 @@ if __name__ == '__main__':
     dante_odb_filename = '/scratch/users/erik/scania_gear_analysis/odb_files/heat_treatment/mesh_' + \
                          mesh + '/dante_results.odb'
     pickle_directory = '/scratch/users/erik/scania_gear_analysis/pickles/tooth_root_fatigue_analysis/mesh_' \
-                       + mesh + '/dante'
+                       + mesh + '/dante/'
 
     element_labels = get_list_from_set_file('../planetary_gear/input_files/gear_models/planet_gear/mesh_' + mesh + '/' +
                                             element_set_name + '.inc')
 
     add_element_set(dante_odb_filename, element_set_name, element_labels, 'tooth_right')
     add_element_set(dante_odb_filename, element_set_name, element_labels, 'tooth_left')
-    write_pickle_for_case_depth(dante_odb_filename, 0.5, '', 'tooth_root_volume_elements')
+    for cd in [0.5, 0.8, 1.1, 1.4]:
+        pickle_name = pickle_directory + 'data_' + str(cd).replace('.', '_') + '.pkl'
+        write_pickle_for_case_depth(dante_odb_filename, cd, '', 'tooth_root_volume_elements')
