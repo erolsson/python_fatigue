124f06589737b09e5d549cb72de3f080869f14c3
erolsson <erolsson@kth.se>
2018-08-20 14:45:28 +0200

    Frist version

---
+++ abaqus/write_pulsator_mechanical_pickles.py
@@ -0,0 +1,35 @@
+from odbAccess import *
+
+import numpy as np
+import pickle
+
+from odb_io_functions import add_element_set
+from odb_io_functions import read_field_from_odb
+from odb_io_functions import cylindrical_system_z
+
+from write_nodal_coordinates import get_list_from_set_file
+
+
+if __name__ == '__main__':
+    mesh = '1x'
+    element_set_name = 'tooth_root_volume_elements'
+    pulsator_odb_filename = '/scratch/users/erik/scania_gear_analysis/odb_files/pulsator/mesh_' + \
+                         mesh + '/pulsator_stresses.odb'
+
+    pickle_directory = '/scratch/users/erik/scania_gear_analysis/pickles/tooth_root_fatigue_analysis/mesh_' \
+                       + mesh + '/pulsator/'
+
+    element_labels = get_list_from_set_file('../planetary_gear/input_files/gear_models/planet_gear/mesh_' + mesh + '/' +
+                                            element_set_name + '.inc')
+
+    add_element_set(dante_odb_filename, element_set_name, element_labels, 'tooth_left')
+
+    pulsator_odb = openOdb(dante_odb_filename)
+    step_names = pulsator_odb.steps.keys()
+    pulsator_odb.close()
+    for step_name in step_names:
+        min_load = read_field_from_odb('S', pulsator_odb_filename, element_set_name, step_names, 0,
+                                       instance_name='tooth_left')
+        max_load = read_field_from_odb('S', pulsator_odb_filename, element_set_name, step_names, 1,
+                                       instance_name='tooth_left')
+        print np.max(max_load)
