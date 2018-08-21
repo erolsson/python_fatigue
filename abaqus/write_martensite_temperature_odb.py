ffcd841a2a53f81d3b05083277b168ef639f2b53
erolsson <erolsson@kth.se>
2018-08-21 21:36:27 +0200

    Frist version

--- abaqus/write_martensite_temperature_odb.py
+++ abaqus/write_martensite_temperature_odb.py
@@ -3,13 +3,13 @@ import odbAccess
 from abaqusConstants import *
 
 odb = odbAccess.openOdb('/scratch/users/erik/scania_gear_analysis/odb_files/heat_treatment/mesh_1x/martensite.odb',
-                        readOnly=False)
+                        new_step=odb.Step(name='martensite_data', description='', domain=TIME, timePeriod=1)                        readOnly=False)
+new_step = odb.Step(name='martensite_data', description='', domain=TIME, timePeriod=1)
 for cd in ['0_5', '0_8', '1_1']:
     step_name = 'dante_results_' + str(cd).replace('.', '_')
     step = odb.steps[step_name]
     frame = step.frames[0]
     field = frame.fieldOutputs['SDV_Q_MARTENSITE']
-    new_step = odb.Step(name='martensite_data', description='', domain=TIME, timePeriod=1)
     start_frame = new_step.Frame(incrementNumber=0, frameValue=0, description='')
     end_frame = new_step.Frame(incrementNumber=1, frameValue=1., description='')
     new_field = field
