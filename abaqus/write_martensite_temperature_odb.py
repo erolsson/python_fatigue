1e80505433f07535866767225c036daae790595c
erolsson <erolsson@kth.se>
2018-08-21 21:33:22 +0200

    Frist version

--- abaqus/write_martensite_temperature_odb.py
+++ abaqus/write_martensite_temperature_odb.py
@@ -8,7 +8,7 @@ for cd in ['0_5', '0_8', '1_1']:
     step_name = 'dante_results_' + str(cd).replace('.', '_')
     step = odb.steps[step_name]
     frame = step.frames[0]
-    field = frame.fieldOutputs['Q_MARTENSITE']
+    field = frame.fieldOutputs['SDV_Q_MARTENSITE']
     new_step = odb.Step(name='martensite_data', description='', domain=TIME, timePeriod=1)
     start_frame = new_step.Frame(incrementNumber=0, frameValue=0, description='')
     end_frame = new_step.Frame(incrementNumber=1, frameValue=1., description='')
