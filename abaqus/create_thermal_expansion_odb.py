206c2404cbeccfd793452592836302053f505931
erolsson <erolsson@kth.se>
2018-08-22 17:08:38 +0200

    Frist version

--- abaqus/create_thermal_expansion_odb.py
+++ abaqus/create_thermal_expansion_odb.py
@@ -31,9 +31,9 @@ def create_node_field_from_element_field(fields, odb_file_name, element_set_name
     return data_dict
 
 
-def expansion(martensite, carbon):
-    # dv = (4.64 - 53*carbon)*martensite + (4.64 - 143*carbon)*lower_bainite + (4.64-221*carbon)*uppper_bainite
-    dv = (3.216+85.9*carbon + 343*carbon*carbon)*martensite
+def expansion(martensite, carbon, lower_bainite):
+    dv = 168*carbon*martensite + 78*carbon*lower_bainite
+    # dv = (3.216+85.9*carbon + 343*carbon*carbon)*martensite
     return dv/300
 
 
@@ -72,8 +72,9 @@ if __name__ == '__main__':
 
         m = odb.steps[dante_step_name].frames[0].fieldOutputs['SDV_Q_MARTENSITE']
         c = odb.steps[dante_step_name].frames[0].fieldOutputs['SDV_CARBON']
+        lb = odb.steps[dante_step_name].frames[0].fieldOutputs['SDV_LBAINITE']
 
-        expansion_strain = expansion(m, c)
+        expansion_strain = expansion(m, c, lb)
         start_frame = expansion_step.frames[0]
         end_frame = expansion_step.frames[1]
 
