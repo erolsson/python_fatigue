9fef16a484c62245b1fce88685a78cd7963e81c3
erolsson <erolsson@kth.se>
2018-08-22 13:02:47 +0200

    Frist version

--- abaqus/create_thermal_expansion_odb.py
+++ abaqus/create_thermal_expansion_odb.py
@@ -6,16 +6,18 @@ import numpy as np
 from odb_io_functions import read_field_from_odb
 
 
-def create_node_field_from_element_field(field, odb_file_name, element_set_name, step_name, frame_number,
+def create_node_field_from_element_field(fields, odb_file_name, element_set_name, step_name, frame_number,
                                          instance_name):
-    element_field, node_labels, _ = read_field_from_odb(field, odb_file_name, element_set_name=element_set_name,
-                                                        step_name=step_name, frame_number=frame_number,
-                                                        instance_name=instance_name, get_position_numbers=True)
+    for i, field in enumerate(fields):
+        element_field, node_labels, _ = read_field_from_odb(field, odb_file_name, element_set_name=element_set_name,
+                                                            step_name=step_name, frame_number=frame_number,
+                                                            instance_name=instance_name, get_position_numbers=True)
 
-    nodal_data = {node_label: [] for node_label in node_labels}
-    for field_value, node_label in zip(element_field, node_labels):
-        nodal_data[node_label].append(field_value[0])
-    print nodal_data.items()[5325]
+        if i == 0:
+            nodal_data = {node_label: {f: [] for f in fields} for node_label in node_labels}
+        for field_value, node_label in zip(element_field, node_labels):
+            nodal_data[node_label][field].append(field_value[0])
+        print nodal_data.items()[5325]
 
 
 if __name__ == '__main__':
