ec7fba824a5dfaafa8070080e75ded49bdf1a660
erolsson <erolsson@kth.se>
2018-08-24 15:02:09 +0200

    ""

--- abaqus/transfer_planet_stresses.py
+++ abaqus/transfer_planet_stresses.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from collections import namedtuple
 from subprocess import Popen
 
@@ -44,7 +45,7 @@ def transfer_gear_stresses(from_odb_name, to_odb_name):
 
 if __name__ == '__main__':
     odb_directory = os.path.expanduser('~/scania_gear_analysis/odb_files/planet_gear/')
-    torque = 1200
+    torque = int(sys.argv[1])
 
     input_file_name = '/scratch/users/erik/python_fatigue/planetary_gear/' \
                       'input_files/planet_sun/planet_dense_geom_xpos.inc'
