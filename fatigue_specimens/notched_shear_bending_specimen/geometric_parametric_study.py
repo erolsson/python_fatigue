9e332da5d2d9849fe8a9babe502cea21387c2b4d
erolsson <erolsson@kth.se>
2018-08-28 17:04:05 +0200

    ""

--- fatigue_specimens/notched_shear_bending_specimen/geometric_parametric_study.py
+++ fatigue_specimens/notched_shear_bending_specimen/geometric_parametric_study.py
@@ -1,13 +1,13 @@
 from mechanical_analysis import perform_mechanical_analysis
 from mechanical_analysis import SpecimenGeometry
 
-SpecimenGeometry(L=100, R=3.5, q=30, t=6, h=24, h1=12, CD=2, delta=-2,
-                 name='BendingSpecimenPart')
+speciimen = SpecimenGeometry(L=100, R=3.5, q=30, t=6, h=24, h1=12, CD=2, delta=-2,
+                             name='BendingSpecimenPart')
 
 simulation_name = ''
 # namedtuple._asdict() acceptable to use
 # noinspection PyProtectedMember
-for key, value in SpecimenGeometry._asdict().iteritems():
+for key, value in speciimen._asdict().iteritems():
     simulation_name += key + '=' + str(value) + '_'
 simulation_name = simulation_name[:, -1]
 print simulation_name
