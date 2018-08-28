3507ba40af2068a0ff33275dd1d435f82617ffed
erolsson <erolsson@kth.se>
2018-08-28 17:17:46 +0200

    ""

--- fatigue_specimens/notched_shear_bending_specimen/geometric_parametric_study.py
+++ fatigue_specimens/notched_shear_bending_specimen/geometric_parametric_study.py
@@ -6,7 +6,7 @@ from mechanical_analysis import SpecimenGeometry
 specimen = SpecimenGeometry(L=100, R=3.5, q=30, t=6, h=24, h1=12, CD=2, delta=-2,
                             name='BendingSpecimenPart')
 
-simulation_name = os.path.expanduser('~/scania_gear_analysis/abaqus/fatigue_specimens/shear_bending_specimen/')
+simulation_name = os.path.expanduser('~/scania_gear_analysis/abaqus/fatigue_specimens/notched_shear_bending_specimen/')
 # namedtuple._asdict() acceptable to use
 # noinspection PyProtectedMember
 for key, value in specimen._asdict().iteritems():
