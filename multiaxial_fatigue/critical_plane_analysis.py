d4af86b2e8f9120eb2f68319c7570aa95c1aa04b
erolsson <erolsson@kth.se>
2018-09-03 11:02:36 +0200

    ""

---
+++ multiaxial_fatigue/critical_plane_analysis.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+import matplotlib.pyplot as plt
+import matplotlib.style
+
+matplotlib.style.use('classic')
+plt.rc('text', usetex=True)
+plt.rc('font', serif='Computer Modern Roman')
+plt.rcParams.update({'font.size': 20})
+plt.rcParams['text.latex.preamble'] = [r"\usepackage{amsmath}"]
+plt.rc('font', **{'family': 'serif', 'serif': ['Computer Modern Roman'],
+                  'monospace': ['Computer Modern Typewriter']})
