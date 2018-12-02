2f7141c425ba99967eac2ffb53d88d3eb71bc59d
erolsson <erolsson@kth.se>
2018-12-02 16:57:05 +0100

    ""

--- materials/gear_materials.py
+++ materials/gear_materials.py
@@ -40,34 +40,65 @@ class SS2506MaterialTemplate:
     # Phase transformation data
     @staticmethod
     def _trans_strain_martensite(temperature, carbon):
-        return temperature*(1.6369e-5 - 2.1344e-3*carbon) - 6.12e-3 + 1.05*carbon
+        temperature += 273.15
+        t, c = np.meshgrid(temperature, carbon)
+        e = t*(1.6369e-5 - 2.1344e-3*c) - 6.12e-3 + 1.05*c
+        return np.squeeze(e)
 
     @staticmethod
     def _thermal_exp_martensite(temperature, carbon):
-        return 1.6369e-5 - 2.1344e-3*carbon
+        temperature += 273.15
+        t, c = np.meshgrid(temperature, carbon)
+        a = 1.6369e-5 - 2.1344e-3*c
+        return np.squeeze(a)
 
     @staticmethod
     def _trans_strain_austenite(temperature, carbon):
-        return temperature*2.4e-5 - 0.017961 + 0.33041*carbon
+        temperature += 273.15
+        t, c = np.meshgrid(temperature, carbon)
+        e = t*2.4e-5 - 0.017961 + 0.33041*c
+        return np.squeeze(e)
 
     @staticmethod
     def _thermal_exp_austenite(temperature, carbon):
-        return 2.4e-5 + 0*temperature
+        temperature += 273.15
+        t, c = np.meshgrid(temperature, carbon)
+        a = 2.4e-5 + 0*t
+        return np.squeeze(a)
 
     @staticmethod
     def _trans_strain_fp(temperature, carbon):
-        return 2.2520e-9*temperature**2 + 1.1643e-5*temperature - 3.6923e-3
+        temperature += 273.15
+        t, c = np.meshgrid(temperature, carbon)
+        e = 2.2520e-9*t**2 + 1.1643e-5*t - 3.6923e-3
+        return np.squeeze(e)
 
     @staticmethod
     def _thermal_exp_fp(temperature, carbon):
-        return 2*2.2520e-9*temperature + 1.1643e-5
+        temperature += 273.15
+        t, c = np.meshgrid(temperature, carbon)
+        a = 2*2.2520e-9*t + 1.1643e-5
+        return np.squeeze(a)
 
     def _trans_strain_bainite(self, temperature, carbon):
-        return self._thermal_exp_fp(temperature, carbon) - 1.3315e-3 + 0.10908*carbon
+        _, c = np.meshgrid(temperature, carbon)
+        e = self._thermal_exp_fp(temperature, carbon) - 1.3315e-3 + 0.10908*c
+        return np.squeeze(e)
 
     def _thermal_exp_bainite(self, temperature, carbon):
-        return self._thermal_exp_fp(temperature, carbon)
+        a = self._thermal_exp_fp(temperature, carbon)
+        return np.squeeze(a)
 
 
 # SS2506 = SS2506MaterialTemplate(swa=138, swb=0.71, mb=11.06e6)
 SS2506 = SS2506MaterialTemplate(swa=240, swb=0.482, mb=11.06e6)
+
+if __name__ == '__main__':
+    # Testing the transformation functions
+    # carb = np.linspace(0.002, 0.0012, 10)
+    # temp = np.linspace(0, 1000, 100)
+    carb = 0.004
+    temp = 300
+
+    for trans_strain_func in SS2506.transformation_strain:
+        print trans_strain_func, trans_strain_func(temp, carb)
