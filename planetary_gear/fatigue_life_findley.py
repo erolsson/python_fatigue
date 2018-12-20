bbcb2ab2212167f20e3e1af67dccc6711a117e6c
erolsson <erolsson@kth.se>
2018-12-20 11:19:33 +0100

    ""

--- planetary_gear/fatigue_life_findley.py
+++ planetary_gear/fatigue_life_findley.py
@@ -4,6 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 
+from planetary_gear.test_results import TestResults
+
 matplotlib.style.use('classic')
 plt.rc('text', usetex=True)
 plt.rc('font', serif='Computer Modern Roman')
@@ -17,5 +19,8 @@ mesh = '1x'
 test_directory = os.path.expanduser('~/scania_gear_analysis/experimental_data/pulsator_testing/')
 case_depths = [0.5, 0.8, 1.1, 1.4]
 
-for cd in case_depths:
-    pass
+for case_depth in case_depths:
+    name = test_directory + 'case_depth_' + str(case_depth)
+    name = name.replace('.', '_')
+    test_data = TestResults(name + '.dat')
+    print test_data.get_test_results()[32.]
