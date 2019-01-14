a84b85eb4092c4c9081fed122df3098254514b11
erolsson <erolsson@kth.se>
2019-01-14 18:03:21 +0100

    ""

--- materials/hardess_convertion_functions.py
+++ materials/hardess_convertion_functions.py
@@ -5,6 +5,10 @@ def HRC2HV(HRC):
     return (223.*HRC+14500)/(100-HRC)
 
 
+def HV2HRC(HV):
+    return (100.*HV - 14500)/(HV+223)
+
+
 if __name__ == '__main__':
     HRC_data = np.arange(59., 65., 1.)
     for HRC in HRC_data:
