2781e5c816c7b6180bfc887657bf6a0d911f696d
erolsson <erolsson@kth.se>
2019-02-05 14:51:25 +0100

    ""

--- fatigue_specimens/UTMIS/findley_analysis_utmis_specimens.py
+++ fatigue_specimens/UTMIS/findley_analysis_utmis_specimens.py
@@ -19,10 +19,10 @@ findley_pickle_directory = '/scratch/users/erik/scania_gear_analysis/pickles/utm
 if not os.path.isdir(findley_pickle_directory):
     os.makedirs(findley_pickle_directory)
 
-load_levels = {'smooth': {-1.: np.array([760]),
-                          0.0: np.array([424])},
-               'notched': {-1.: np.array([439]),
-                           0.0: np.array([237])}}
+load_levels = {'smooth': {-1.: np.array([760.]),
+                          0.0: np.array([424.])},
+               'notched': {-1.: np.array([439.]),
+                           0.0: np.array([237.])}}
 
 with open(dante_pickle_directory + 'unit_load_' + specimen + '.pkl') as pickle_handle:
     mechanical_data = pickle.load(pickle_handle)
@@ -49,6 +49,7 @@ for amplitude_stress in load_levels[specimen][R]:
 
     findley_stress = findley_data[:, 2]
     print "Maximum Findley stress", np.max(findley_stress), 'MPa'
-    findley_pickle_name = 'findley_' + specimen + '_s=' + str(amplitude_stress) + 'MPa.pkl'
+    findley_pickle_name = 'findley_' + specimen + 'R=' + str(int(R)) + '_' + _ + 's=' + str(int(amplitude_stress)) + \
+                          'MPa.pkl'
     with open(findley_pickle_directory + findley_pickle_name, 'w') as pickle_handle:
         pickle.dump(findley_stress, pickle_handle)
