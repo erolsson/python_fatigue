1550b3b5681013113cc57ecbd31d80d99fcf5193
erolsson <erolsson@kth.se>
2019-02-09 15:33:35 +0100

    ""

--- abaqus/load_findley_pickles_to_odb_utmis.py
+++ abaqus/load_findley_pickles_to_odb_utmis.py
@@ -36,13 +36,14 @@ if __name__ == '__main__':
         findley_parameter_directories = [os.path.normpath(directory).split(os.sep)[-1]
                                          for directory in findley_parameter_directories]
         findley_parameters = [directory[5:] for directory in findley_parameter_directories]
+        for R in [0, -1]:
+            for findley_parameter in findley_parameters:
 
-        for findley_parameter in findley_parameters:
-            for R in [0, -1]:
                 pickle_filenames = glob.glob(findley_pickle_directory + 'a800=' + findley_parameter + '/findley_' +
                                              specimen + '_R=' + str(int(R)) + '_' + 's=*.pkl')
                 stress_amps = [filename[-10:-7] for filename in pickle_filenames]
                 for pickle_file, load in zip(pickle_filenames, stress_amps):
+
                     print 'loading pickle file', pickle_file
                     with open(pickle_file, 'r') as pickle_handle:
                         findley_stress = pickle.load(pickle_handle)
