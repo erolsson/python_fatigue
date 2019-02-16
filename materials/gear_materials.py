98fa59cbf7f1667d0bac7d7213a45e41e3da90be
erolsson <erolsson@kth.se>
2019-02-16 14:56:48 +0100

    "Commenting code"

--- materials/gear_materials.py
+++ materials/gear_materials.py
@@ -38,8 +38,8 @@ class SS2506MaterialTemplate:
         return self.sw_par[0] + self.sw_par[1]*steel_properties.HV
 
     def weibull_m(self, steel_properties):
-        return self.m_par[0]/steel_properties.HV**2
-
+        return  self.m_par[0]
+        # return self.m_par[0]/steel_properties.HV**2
 
     # Phase transformation data
     def _trans_strain_martensite(self, temperature, carbon):
