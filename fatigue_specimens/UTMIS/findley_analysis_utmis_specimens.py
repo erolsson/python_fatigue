c0def096cea391daea80c285eb4085f5be964526
erolsson <erolsson@kth.se>
2019-02-17 12:59:25 +0100

    "Commenting code"

--- fatigue_specimens/UTMIS/findley_analysis_utmis_specimens.py
+++ fatigue_specimens/UTMIS/findley_analysis_utmis_specimens.py
@@ -9,8 +9,9 @@ from multiaxial_fatigue.findley_evaluation_functions import evaluate_findley
 specimen = sys.argv[1]
 R = float(sys.argv[2])
 
-dante_pickle_directory = '/scratch/users/erik/scania_gear_analysis/pickles/utmis_specimens/heat_treatment_data/dante/'
-mechanical_pickle_directory = '/scratch/users/erik/scania_gear_analysis/pickles/utmis_specimens/stresses/'
+dante_pickle_directory = os.path.expanduser('~/scania_gear_analysis/pickles/utmis_specimens/heat_treatment_data/dante/')
+mechanical_pickle_directory = os.path.expanduser('~/scania_gear_analysis/pickles/utmis_specimens/stresses/')
+geometry_pickle_directory = os.path.expanduser('~/scania_gear_analysis/pickles/utmis_specimens/geometry/')
 
 load_levels = {'smooth': {-1.: np.array([737., 774, 820]),
                           0.0: np.array([425., 440])},
@@ -20,9 +21,17 @@ load_levels = {'smooth': {-1.: np.array([737., 774, 820]),
 with open(mechanical_pickle_directory + 'unit_load_' + specimen + '.pkl') as pickle_handle:
     mechanical_data = pickle.load(pickle_handle)
 
+with open(geometry_pickle_directory + 'nodal_coordinates_' + specimen + '.pkl') as pickle_handle:
+    nodal_coordinates = pickle.load(pickle_handle)
+
+
+distance_to_monitor_node = 0*nodal_coordinates + nodal_coordinates
+distance_to_monitor_node[:, 1] -= 2.5
+monitor_node_idx = np.argmin(np.sum(np.abs(distance_to_monitor_node), 1))
+
 print "======== Unit load stress state =========="
-print "The maximum stress in the x-direction is ", np.max(mechanical_data[:, 0]), "MPa"
-print "The minimum stress in the x-direction is ", np.min(mechanical_data[:, 0]), "MPa"
+print "The maximum stress in the x-direction is ", mechanical_data[monitor_node_idx, 0], "MPa"
+print "The minimum stress in the x-direction is ", mechanical_data[monitor_node_idx, 0], "MPa"
 
 with open(dante_pickle_directory + 'data_utmis_' + specimen + '.pkl') as pickle_handle:
     dante_data = pickle.load(pickle_handle)
@@ -37,21 +46,21 @@ for amplitude_stress in load_levels[specimen][R]:
     max_stress = (mean_stress + amplitude_stress)*mechanical_data
     min_stress = (mean_stress - amplitude_stress)*mechanical_data
 
-    print "The maximum stress in the x-direction is ", np.max(max_stress[:, 0]), "MPa"
-    print "The minimum stress in the x-direction is ", np.min(min_stress[:, 0]), "MPa"
+    print "The maximum stress in the x-direction is ", max_stress[monitor_node_idx, 0], "MPa"
+    print "The minimum stress in the x-direction is ", min_stress[monitor_node_idx, 0], "MPa"
 
     stress_history = np.zeros((2, n, 6))
     stress_history[0, :, :] = min_stress + dante_data['S']
     stress_history[1, :, :] = max_stress + dante_data['S']
     print "======== Combined stress state =========="
-    print "The maximum stress in the x-direction is ", np.max(stress_history[1, :, 0]), "MPa"
-    print "The minimum stress in the x-direction is ", np.min(stress_history[0, :, 0]), "MPa"
+    print "The maximum stress in the x-direction is ", stress_history[1, monitor_node_idx, 0], "MPa"
+    print "The minimum stress in the x-direction is ", stress_history[0, monitor_node_idx, 0], "MPa"
     for a800 in np.arange(0.6, 1.7, 0.1):
         print '======================================================================================================='
         print '          Analyzing a800 =', a800
         print '======================================================================================================='
-        findley_pickle_directory = '/scratch/users/erik/scania_gear_analysis/pickles/utmis_specimens/stresses/findley/'\
-                                   + 'a800=' + str(a800).replace('.', '_') + '/'
+        findley_pickle_directory = os.path.expanduser('~/scania_gear_analysis/pickles/utmis_specimens/stresses/findley/'
+                                                      + 'a800=' + str(a800).replace('.', '_') + '/')
 
         if not os.path.isdir(findley_pickle_directory):
             os.makedirs(findley_pickle_directory)
@@ -62,7 +71,8 @@ for amplitude_stress in load_levels[specimen][R]:
 
         print "The findley parameter coefficients are a={} and b={}".format(a, b)
         findley_k = a + b*HV
-        print "The maximum value of the findley parameter is", np.max(findley_k), "and the minimum is", np.min(findley_k)
+        print "The maximum value of the findley parameter is", np.max(findley_k), "and the minimum is", \
+            np.min(findley_k)
         findley_data = evaluate_findley(combined_stress=stress_history, a_cp=findley_k, worker_run_out_time=80000,
                                         num_workers=8, chunk_size=1000, search_grid=10)
 
