e3c0a254edd30ced5789c289c05857f51fb8d1f9
erolsson <erolsson@kth.se>
2019-02-26 15:45:46 +0100

    Added utmis specimens

--- fatigue_specimens/UTMIS/findley_point_stress.py
+++ fatigue_specimens/UTMIS/findley_point_stress.py
@@ -1,22 +1,28 @@
 import numpy as np
-import matplotlib as mlt
-mlt.use('Agg')
+
 import matplotlib.pyplot as plt
 import matplotlib.style
 
-from weakest_link_functions import evaluated_findley_parameters
-from weakest_link_functions import Simulation
+matplotlib.style.use('classic')
+plt.rc('text', usetex=True)
+plt.rc('font', serif='Computer Modern Roman')
+plt.rcParams.update({'font.size': 20})
+plt.rcParams['text.latex.preamble'] = [r"\usepackage{amsmath}"]
+plt.rc('font', **{'family': 'serif', 'serif': ['Computer Modern Roman'],
+                  'monospace': ['Computer Modern Typewriter']})
 
 if __name__ == '__main__':
-    simulations = [Simulation(specimen='smooth', R=-1., stress=737., failures=1, run_outs=4),
-                   Simulation(specimen='smooth', R=-1., stress=774., failures=4, run_outs=2),
-                   Simulation(specimen='smooth', R=-1., stress=820., failures=3, run_outs=1),
-                   Simulation(specimen='smooth', R=0., stress=425., failures=2, run_outs=3),
-                   Simulation(specimen='smooth', R=0., stress=440., failures=4, run_outs=2),
-                   Simulation(specimen='notched', R=-1., stress=427., failures=2, run_outs=4),
-                   Simulation(specimen='notched', R=-1., stress=450., failures=5, run_outs=3),
-                   Simulation(specimen='notched', R=0., stress=225., failures=2, run_outs=3),
-                   Simulation(specimen='notched', R=0., stress=240., failures=2, run_outs=4),
-                   Simulation(specimen='notched', R=0., stress=255., failures=4, run_outs=0)]
 
-    for simulation
+    plt.plot([-277, -277+430*1.03], [760*1.03, 430*1.03], '-gx', ms=12)
+
+    plt.plot([-376, -376+250*1.89], [440*1.89, 250*1.89], '-rx', ms=12)
+
+    sm = np.linspace(-400, 200, 1000)
+
+    k = 1.3
+    sF = 580*(k + np.sqrt(1+k**2))/2
+    print sF
+    sa = 2*(-k*sF + np.sqrt(sF**2 + k**2*sF**2 - k*sF*sm))
+    plt.plot(sm, sa)
+
+    plt.show()
