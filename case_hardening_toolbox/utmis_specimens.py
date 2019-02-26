499d1705b3ab36ef6bb5bdb73c3cbf11c59f2283
erolsson <erolsson@kth.se>
2019-02-26 20:41:15 +0100

    Added utmis specimens

--- case_hardening_toolbox/utmis_specimens.py
+++ case_hardening_toolbox/utmis_specimens.py
@@ -35,7 +35,7 @@ Simulation = namedtuple('Simulation', ['simulation_directory', 'times', 'tempera
 current_directory = os.getcwd()
 specimen_name = 'utmis_' + specimen_type
 simulations = [Simulation(simulation_directory=specimen_name + '_two_stage',
-                          times=[210], temperatures=[840.], carbon=[0.75], tempering=(200, 7200))]
+                          times=[240], temperatures=[840.], carbon=[0.75], tempering=(200, 7200))]
 
 # This is the main directory where all simulation folders will be placed
 simulation_directory = os.path.expanduser('~/scania_gear_analysis/abaqus/' + specimen_name + '_tempering_2h_200C/')
