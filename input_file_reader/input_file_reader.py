3f604174e40b6bca7376d38fd7a96e20159f7f86
erolsson <erolsson@kth.se>
2019-02-26 16:24:11 +0100

    Added utmis specimens

--- input_file_reader/input_file_reader.py
+++ input_file_reader/input_file_reader.py
@@ -54,7 +54,7 @@ class InputFileReader:
             node_string = '\t'
             for n_data in node:
                 node_string = node_string + str(n_data) + ','
-            file_lines.append(node_string[:, -1])
+            file_lines.append(node_string[:-1])
         for element_type, element_data in self.elements.iteritems():
             file_lines.append('*ELEMENT, TYPE=' + element_type_prefix + element_type + ', ELSET=ALL_ELEMENTS', )
             for element in element_data:
