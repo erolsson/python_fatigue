f0ffd13d7ea32a2f064a0a34ab225415698ab201
erolsson <erolsson@kth.se>
2019-02-27 13:22:49 +0100

    Added utmis specimens

--- fatigue_specimens/UTMIS/findley_point_stress.py
+++ fatigue_specimens/UTMIS/findley_point_stress.py
@@ -15,13 +15,14 @@ if __name__ == '__main__':
 
     plt.plot([-246, -246+430*1.03], [760*1.03, 430*1.03], '-gx', ms=12)
 
-    plt.plot([-284, -284+250*1.89], [440*1.89, 250*1.89], '-rx', ms=12)
+    plt.plot([-293, -293+250*1.89], [440*1.89, 250*1.89], '-rx', ms=12)
 
     sm = np.linspace(-400, 200, 1000)
 
     k = 1.3
-    sF = 580*(k + np.sqrt(1+k**2))/2
-    print sF
+    sF = 600*(k + np.sqrt(1+k**2))/2
+    sF = 610*(k + np.sqrt(1 + k ** 2)) / 2
+    print 580*(k + np.sqrt(1+k**2))/2, 610*(k + np.sqrt(1+k**2))/2
     sa = 2*(-k*sF + np.sqrt(sF**2 + k**2*sF**2 - k*sF*sm))
     plt.plot(sm, sa)
 
