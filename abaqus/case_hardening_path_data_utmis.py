ee3a53730874734fe10492306a04b5440ff9264b
erolsson <erolsson@kth.se>
2019-02-28 21:27:28 +0100

    Added utmis specimens

--- abaqus/case_hardening_path_data_utmis.py
+++ abaqus/case_hardening_path_data_utmis.py
@@ -10,13 +10,19 @@ from visualization import *
 
 if __name__ == '__main__':
     dante_odb_path = '/scratch/users/erik/scania_gear_analysis/odb_files/heat_treatment/utmis_specimens/'
-    odb_file_name = dante_odb_path + 'utmis_smooth.odb'
+
     path_points = np.ones((100, 3))*1e-3
     path_points[:, 1] = np.linspace(2.5-1e-3, 0, 100)
     path = Path('path', path_points, np.array([1, 0, 0]))
     pickle_directory = '/scratch/users/erik/scania_gear_analysis/pickles/utmis_specimens/heat_treatment_data/'
     if not os.path.isdir(pickle_directory):
         os.makedirs(pickle_directory)
-
-    write_case_hardening_data_along_path(odb_file_name, path, pickle_directory + '/utmis_smooth_dante_path.pkl',
-                                         session=session)
+    for specimen in ['smooth', 'notched']:
+        odb_file_name = dante_odb_path + 'utmis_' + specimen + '.odb'
+        for carb in [0.75, 0.8]:
+            for temp in [180, 200]:
+                pickle_name = 'utmis_' + specimen + 'dante_path_tempering_2h_' + str(temp) + '_' + \
+                              str(carb).replace('.', '_') + 'C' + '.pkl'
+                step_name = 'dante_results_tempering_2h_' + str(temp) + '_' + str(carb).replace('.', '_') + 'C'
+                write_case_hardening_data_along_path(odb_file_name, path, pickle_name, step_name=step_name,
+                                                     session=session)
