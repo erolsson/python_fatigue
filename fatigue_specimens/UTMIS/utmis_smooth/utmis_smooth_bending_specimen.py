59fbfbb078979b640371e849f6747f7e01d3bd38
erolsson <erolsson@kth.se>
2019-03-03 15:35:07 +0100

    Added utmis specimens

--- fatigue_specimens/UTMIS/utmis_smooth/utmis_smooth_bending_specimen.py
+++ fatigue_specimens/UTMIS/utmis_smooth/utmis_smooth_bending_specimen.py
@@ -206,7 +206,7 @@ class SmoothBendingSpecimenClass:
             edges.append(part.edges.findAt((x, y_coordinates[i] - self.case_mesh_thickness, z)))
             edges.append(part.edges.findAt((x, y_coordinates[i],                            z)))
 
-        edges.append(part.edges.findAt((self.length/2, 0, 1.9)))
+        edges.append(part.edges.findAt((self.length/2, 0, z)))
         edges1, edges2 = edges_direction_part(self.fatigue_part, edges)
 
         if flip is True:
