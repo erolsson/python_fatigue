844e7a825332cf858fa0823d83abd77a23c83928
erolsson <erolsson@kth.se>
2019-03-04 10:11:22 +0100

    "Commenting code"

--- case_hardening_toolbox/utmis_specimens.py
+++ case_hardening_toolbox/utmis_specimens.py
@@ -34,11 +34,11 @@ Simulation = namedtuple('Simulation', ['simulation_directory', 'times', 'tempera
 
 current_directory = os.getcwd()
 specimen_name = 'utmis_' + specimen_type
-simulations = [Simulation(simulation_directory=specimen_name + '_oil60_0_8C/',
-                          times=[270], temperatures=[840.], carbon=[0.8], tempering=(200, 7200))]
+simulations = [Simulation(simulation_directory=specimen_name + '_oil60_0_75C/',
+                          times=[270], temperatures=[840.], carbon=[0.75], tempering=(180, 7200))]
 
 # This is the main directory where all simulation folders will be placed
-simulation_directory = os.path.expanduser('~/scania_gear_analysis/abaqus/' + specimen_name + '_tempering_2h_200C/')
+simulation_directory = os.path.expanduser('~/scania_gear_analysis/abaqus/' + specimen_name + '_tempering_2h_180C/')
 
 # In this directory all common files for all heat treatment simulations will be placed
 include_file_directory = simulation_directory + 'include_files/'
