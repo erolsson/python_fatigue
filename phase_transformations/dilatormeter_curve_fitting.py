0fe5628e1b1c51f636f4f0bc607ee0a9fb0a9b93
erolsson <erolsson@kth.se>
2019-03-13 21:50:58 +0100

    "Commenting code"

--- phase_transformations/dilatormeter_curve_fitting.py
+++ phase_transformations/dilatormeter_curve_fitting.py
@@ -19,8 +19,8 @@ plt.rc('font', **{'family': 'serif', 'serif': ['Computer Modern Roman'],
 
 def expansion_martensite(par, c, t):
     par[5:] = 0
-    par[4] = (- 1.2678425108258802e-05)/(1.2 - 0.2)
-    par[3] = 1.2678425e-5 - par[4] * 0.2
+    # par[4] = (- 1.2678425108258802e-05)/(1.2 - 0.2)
+    # par[3] = 1.2678425e-5 - par[4] * 0.2
 
     m1, m2, m3, m4, m5, m6, m7, m8 = par
     return m1 + m2*c + m3*c**2 + m4*t + m5*c*t + m6*t**2 + m7*c*t**2 + m8*t**3
@@ -28,8 +28,8 @@ def expansion_martensite(par, c, t):
 
 def heat_expanion_martensite(par, c, t):
     par[5:] = 0
-    par[4] = (- 1.2678425108258802e-05)/(1.2 - 0.2)
-    par[3] = 1.2678425e-5 - par[4]*0.2
+    # par[4] = (- 1.2678425108258802e-05)/(1.2 - 0.2)
+    # par[3] = 1.2678425e-5 - par[4]*0.2
 
     m1, m2, m3, m4, m5, m6, m7, m8 = par
     return m4 + m5*c + 2*m6*t + 2*m7*c*t + 3*m8*t**2
