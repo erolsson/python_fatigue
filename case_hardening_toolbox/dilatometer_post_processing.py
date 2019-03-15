11927844d130205d38043826967359ece3e078e2
erolsson <erolsson@kth.se>
2019-03-15 15:01:43 +0100

    "Commenting code"

---
+++ case_hardening_toolbox/dilatometer_post_processing.py
@@ -0,0 +1,25 @@
+import odbAccess
+
+import pickle
+import sys
+
+import numpy as np
+
+
+name = sys.argv[-1]
+odb = odbAccess.openOdb('oneElement.odb')
+frames = odb.steps['step'].frames
+data = np.zeros((len(frames), 3))  # Time, temp, U3
+
+for i in range(0, len(frames)):
+    frame = frames[i]
+    U = frame.fieldOutputs['U'].values[6]
+    NT = frame.fieldOutputs['NT11'].values[6]
+    data[i, 0] = frame.frameValue
+    data[i, 2] = U.data[2]
+    data[i, 1] = NT.data
+
+pickleHandle = open('data_' + name + '.pkl', 'wb')
+pickle.dump(data, pickleHandle)
+pickleHandle.close()
+odb.close()
