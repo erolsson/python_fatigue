14a8a750069c86acbe12df57050d67c7e542ea6f
erolsson <erolsson@kth.se>
2019-03-17 11:55:27 +0100

    "Commenting code"

--- case_hardening_toolbox/case_hardening_toobox.py
+++ case_hardening_toolbox/case_hardening_toobox.py
@@ -530,7 +530,7 @@ class CaseHardeningToolbox:
         if self.cooldown_data.time is not None and self.cooldown_data.time > 0.:
             self._add_cooldown_step('Cooldown_1', kinematic_mode=-2, time=self.cooldown_data.time,
                                     temperature=self.cooldown_data.temperature)
-            self._add_cooldown_step('Cooldown_1', kinematic_mode=1, time=self.cooldown_data.time,
+            self._add_cooldown_step('Cooldown_2', kinematic_mode=1, time=self.cooldown_data.time,
                                     temperature=20)
 
         if self.tempering_data.time is not None and self.tempering_data.time > 0:
