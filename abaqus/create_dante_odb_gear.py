9a59d031b893d645178f2a727cf52fe5bde4c447
erolsson@kth.se <erolsson@kth.se>
2019-04-12 16:27:09 +0200

    "Calibrating dante data"

--- abaqus/create_dante_odb_gear.py
+++ abaqus/create_dante_odb_gear.py
@@ -65,7 +65,7 @@ def create_dante_step(from_odb_name, to_odb_name, results_step_name):
 
 if __name__ == '__main__':
     dante_odb_path = '/scratch/users/erik/scania_gear_analysis/odb_files/heat_treatment/mesh_1x/'
-    simulation_directory = '/scratch/users/erik/scania_gear_analysis/VBC_gear/U92504_200C_90C_cool_20190412/'
+    simulation_directory = '/scratch/users/erik/scania_gear_analysis/VBC_gear/U92504_200C_2h_90C_cool_20190412/'
 
     input_file_name = '/scratch/users/erik/python_fatigue/planetary_gear/' \
                       'input_files/planet_sun/planet_dense_geom_xpos.inc'
