8676297bba97d3e89037115b48f939eb5fe4bcc0
erolsson@kth.se <erolsson@kth.se>
2019-04-28 18:14:00 +0200

    "Implementing data from JMAT-PRO"

---
+++ fatigue_specimens/UTMIS/surface_stresses.py
@@ -0,0 +1,28 @@
+import os
+import pickle
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+import matplotlib.style
+
+matplotlib.style.use('classic')
+plt.rc('text', usetex=True)
+plt.rc('font', serif='Computer Modern Roman')
+plt.rcParams.update({'font.size': 20})
+plt.rcParams['text.latex.preamble'] = [r"\usepackage{amsmath}"]
+plt.rc('font', **{'family': 'serif', 'serif': ['Computer Modern Roman'],
+                  'monospace': ['Computer Modern Typewriter']})
+
+heat_treatment_pickle_directory = os.path.expanduser('~/scania_gear_analysis/pickles/utmis_specimens/'
+                                                     'heat_treatment_data/')
+
+for specimen in ['smooth', 'notched']:
+    for i, path in enumerate(['y', 'z']):
+        with open(heat_treatment_pickle_directory + 'utmis_' + specimen + '_dante_path_' + path
+                  + '.pkl', 'r') as heat_pickle:
+            heat_treatment_data = pickle.load(heat_pickle)
+        plt.figure(i)
+        plt.plot(heat_treatment_data['r'], heat_treatment_data['S'])
+
+plt.show()
