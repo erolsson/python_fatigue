3aa822c3afe7726da592b13b52802ca19a43f991
erolsson@kth.se <erolsson@kth.se>
2019-04-28 19:12:14 +0200

    "Implementing data from JMAT-PRO"

--- fatigue_specimens/UTMIS/surface_stresses.py
+++ fatigue_specimens/UTMIS/surface_stresses.py
@@ -17,13 +17,25 @@ plt.rc('font', **{'family': 'serif', 'serif': ['Computer Modern Roman'],
 heat_treatment_pickle_directory = os.path.expanduser('~/scania_gear_analysis/pickles/utmis_specimens/'
                                                      'heat_treatment_data/')
 
+mechanical_pickle_directory = os.path.expanduser('~/scania_gear_analysis/pickles/utmis_specimens/'
+                                                     'mechanical_data/')
+
 for specimen in ['smooth', 'notched']:
     for i, path in enumerate(['y', 'z']):
         with open(heat_treatment_pickle_directory + 'utmis_' + specimen + '_dante_path_' + path
                   + '.pkl', 'r') as heat_pickle:
             heat_treatment_data = pickle.load(heat_pickle)
-        print 'utmis_' + specimen + '_dante_path_' + path + '.pkl', i
+
+        with open(mechanical_pickle_directory + 'unit_load_' + specimen + '_path_' + path
+                  + '.pkl', 'r') as mechanical_pickle:
+            mechanical_data = pickle.load(mechanical_pickle)
+
         plt.figure(i)
         plt.plot(heat_treatment_data['r'], heat_treatment_data['S'])
+
+        plt.figure(i+2)
+        plt.plot(mechanical_data[:, 0], mechanical_data[:, 3])
+
+
 
 plt.show()
