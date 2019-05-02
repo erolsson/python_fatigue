dc2a0f1a4a4c88d367b0ed01396d5b8498159616
erolsson@kth.se <erolsson@kth.se>
2019-05-02 15:00:47 +0200

    "Changing bainite hardness"

--- fatigue_specimens/UTMIS/create_files_for_mechanical_analysis.py
+++ fatigue_specimens/UTMIS/create_files_for_mechanical_analysis.py
@@ -25,11 +25,12 @@ simulation_odb = os.path.expanduser('~/scania_gear_analysis/utmis_specimens_U925
 stress, _, labels = read_field_from_odb('S', simulation_odb, step_name='Tempering', frame_number=-1,
                                         position=INTEGRATION_POINT, get_position_numbers=True)
 gp = 1
-with open('residual_stresses_pos', 'w') as stress_file:
+with open('residual_stresses_pos.dat', 'w') as stress_file:
     for i, label in enumerate(labels):
         line = str(label) + ", " + str(gp)
         for comp in stress[i, :]:
             line += ", " + str(comp)
+        stress_file.write(line + '\n')
         gp += 1
         if gp == 9:
             gp = 1
