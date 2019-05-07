ba6114564485987d75b85abc068bc21e589d70f8
erolsson@kth.se <erolsson@kth.se>
2019-05-07 10:56:50 +0200

    "Changing bainite hardness"

--- fatigue_specimens/UTMIS/subroutine.f
+++ fatigue_specimens/UTMIS/subroutine.f
@@ -1,33 +1,21 @@
       MODULE DATA_MOD
             REAL, DIMENSION(:, :), ALLOCATABLE :: STRESS_DATA
+            REAL, DIMENSION(:, :), ALLOCATABLE :: HARDENING_DATA
       END MODULE DATA_MOD
 
-      SUBROUTINE SIGINI(SIGMA,COORDS,NTENS,NCRDS,NOEL,NPT,LAYER,
-     1 KSPT,LREBAR,NAMES)
+      SUBROUTINE UEXTERNALDB(LOP,LRESTART,TIME,DTIME,KSTEP,KINC)
 C
       USE DATA_MOD
       INCLUDE 'ABA_PARAM.INC'
 C
-      REAL s11, s22, s33, s12, s13, s23
-
-      INTEGER ELEM_NUMBER, INTPOINTS, JRCD, ROW, N_LINES
-C
-      DIMENSION SIGMA(NTENS),COORDS(NCRDS)
-C
-      CHARACTER NAMES(2)*80	
-C
-      CHARACTER INSTANCE*80
+      INTEGER N_LINES
       CHARACTER STRESS_FNAME*150
+      DIMENSION TIME(2)
 C
-      LOGICAL NEVER_CALLED
-      DATA NEVER_CALLED/.TRUE./
-
-      CALL MutexInit(1)
-      STRESS_FNAME = '/scratch/users/erik/scania_gear_analysis/' // 
+      IF (LOP .EQ. 0) THEN
+        STRESS_FNAME = '/scratch/users/erik/scania_gear_analysis/' //
      1 'abaqus/utmis_specimens/residual_stresses_pos.dat'
-      CALL MutexLock(1)
-      IF (NEVER_CALLED .EQ. .TRUE.) THEN
-         OPEN(15, FILE=TRIM(ADJUSTL(STRESS_FNAME)))
+        OPEN(15, FILE=TRIM(ADJUSTL(STRESS_FNAME)))
 C     Find out the number of data lines
          N_LINES = 0
          DO
@@ -41,10 +29,27 @@ C     Find out the number of data lines
             READ(15, FMT=*) STRESS_DATA(i, :)
          END DO
          CLOSE(15)
-         NEVER_CALLED = .FALSE.
-      END IF
-      CALL MutexUnlock(1)
-C user coding to define SIGMA(NTENS)		
+        END IF
+      RETURN
+      END
+
+      SUBROUTINE SIGINI(SIGMA,COORDS,NTENS,NCRDS,NOEL,NPT,LAYER,
+     1 KSPT,LREBAR,NAMES)
+C
+      USE DATA_MOD
+      INCLUDE 'ABA_PARAM.INC'
+C
+      REAL s11, s22, s33, s12, s13, s23
+
+      INTEGER ELEM_NUMBER, INTPOINTS, JRCD, ROW
+C
+      DIMENSION SIGMA(NTENS),COORDS(NCRDS)
+C
+      CHARACTER NAMES(2)*80
+C
+      CHARACTER INSTANCE*80
+C
+C user coding to define SIGMA(NTENS)
       s11 = 0.
       s22 = 0.
       s33 = 0.
@@ -72,13 +77,26 @@ C user coding to define SIGMA(NTENS)
             s13 = STRESS_DATA(ROW, 7)
             s23 = STRESS_DATA(ROW, 8)
       END IF
-
       SIGMA(1) = s11
       SIGMA(2) = s22
       SIGMA(3) = s33
       SIGMA(4) = s12
       SIGMA(5) = s13
       SIGMA(6) = s23
+      RETURN
+      END
+
+      SUBROUTINE UFIELD(FIELD,KFIELD,NSECPT,KSTEP,KINC,TIME,NODE,
+     1 COORDS,TEMP,DTEMP,NFIELD)
+C
+      INCLUDE 'ABA_PARAM.INC'
+C
+      DIMENSION FIELD(NSECPT,NFIELD), TIME(2), COORDS(3),
+     1 TEMP(NSECPT), DTEMP(NSECPT)
+C
+
+
+
 
       RETURN
       END
---
+++ materials/SS2506/stress_strain_evaluation.py
@@ -0,0 +1,90 @@
+from collections import namedtuple
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+import matplotlib
+
+matplotlib.style.use('classic')
+plt.rc('text', usetex=True)
+plt.rc('font', serif='Computer Modern Roman')
+plt.rcParams.update({'font.size': 20})
+plt.rcParams['text.latex.preamble'] = [r"\usepackage{amsmath}"]
+plt.rc('font', **{'family': 'serif', 'serif': ['Computer Modern Roman'],
+                  'monospace': ['Computer Modern Typewriter']})
+
+
+class Experiment:
+    def __init__(self, filename, color, hardness, ra, delimiter=None, compression=False):
+        self.color = color
+        sign = 1
+        if compression:
+            sign = -1
+        data = sign*np.genfromtxt(filename, delimiter=delimiter)
+        self.strain = data[:, 0]
+        self.stress = data[:, 1]
+        self.E = 205e3
+        self.hardness = hardness
+        self.ra = ra
+
+    def plot(self):
+        plt.plot(self.strain, self.stress, self.color, lw=2)
+
+    def plastic_strain_data(self, threshold=0.0001):
+        epl = self.strain - self.stress/self.E
+        s = self.stress[epl > threshold]
+        return epl[epl > threshold], s
+
+
+class SS2506Material:
+    def __init__(self, stress_strain_datasets):
+        self.E = 205e3
+        self.plastic_data = np.zeros((1000, len(stress_strain_datasets)))
+
+        self.dp_parameter = 2.15
+        self.dp_ra = 0.2
+        self.hardness_values = np.zeros(len(stress_strain_datasets))
+
+        self.epl = np.linspace(0., 0.025, 1000)
+        for i, dataset in enumerate(stress_strain_datasets):
+            self.plastic_data[:, i] = np.interp(self.epl, dataset.plastic_strain_data()[0],
+                                                dataset.plastic_strain_data()[1])
+            self.hardness_values[i] = dataset.hardness
+
+    def strain(self, stress, hardness, ra, compression=True):
+        print self.plastic_data
+        compressive_plastic_stress = (self.plastic_data[:, 0]
+                                      + (self.plastic_data[:, 1] - self.plastic_data[:, 0])
+                                      / (np.diff(self.hardness_values)) * (hardness - self.hardness_values[0]))
+
+        if not compression:
+            compressive_plastic_stress /= (1 + (self.dp_parameter - 1)*(ra/self.dp_ra))
+
+        return stress/self.E + np.interp(stress, compressive_plastic_stress, self.epl)
+
+
+experiments = [Experiment(filename='compression_data_case_EN.dat', color='b', delimiter=',', compression=True,
+                          hardness=750, ra=20),
+               Experiment(filename='tension_data_case_EN.dat', color='r', delimiter=',',
+                          hardness=750, ra=20),
+               Experiment(filename='tension_data_case_BA.dat', color='g',
+                          hardness=750, ra=20),
+               Experiment(filename='tension_data_core_BA.dat', color='k',
+                          hardness=450, ra=0)]
+
+for experiment in experiments:
+    plt.figure(0)
+    experiment.plot()
+
+    e_pl, stress = experiment.plastic_strain_data()
+    plt.figure(1)
+    plt.plot(e_pl, stress, experiment.color, lw=2)
+
+SS2506 = SS2506Material((experiments[3], experiments[0]))
+
+s = np.linspace(0, 1000, 1000)
+plt.figure(0)
+plt.plot(SS2506.strain(s, 450, 0.6, compression=False), s)
+
+
+plt.show()
