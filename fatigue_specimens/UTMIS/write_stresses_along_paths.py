a05be23c8c138fb1f9ab32146d7bfdf634e8c4a2
erolsson@kth.se <erolsson@kth.se>
2019-05-13 13:49:19 +0200

    implementing Drucker-prager for 2506

--- fatigue_specimens/UTMIS/write_stresses_along_paths.py
+++ fatigue_specimens/UTMIS/write_stresses_along_paths.py
@@ -5,8 +5,10 @@ import sys
 import numpy as np
 
 if __name__ == '__main__':
-    package_directory = os.path.abspath(os.path.join(os.getcwd(), "../.."))
+    package_directory = os.path.expanduser('~/python_fatigue/')
     sys.path.append(package_directory)
+    print sys.path
+
     from path_functions import Path
     from path_functions import create_path
     from path_functions import get_stress_tensors_from_path
