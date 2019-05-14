509f6dc6637fff31ca3ed5d88aa39d5c0ab82624
erolsson@kth.se <erolsson@kth.se>
2019-05-14 13:56:11 +0200

    implementing Drucker-prager for 2506

--- multiaxial_fatigue/findley_tester.py
+++ multiaxial_fatigue/findley_tester.py
@@ -4,16 +4,27 @@ from findley_evaluation_functions import evaluate_findley
 
 # Testing biaxial residual stress plus alternating sxx
 k = 1.
-sa = 760
+sa = 760.
 
 load_hist = np.zeros((2, 1, 6))
-load_hist[:, :, 0] = -254.6
-load_hist[:, :, 1] = -248
+load_hist[:, :, 0] = -250
+load_hist[:, :, 1] = -250
 load_hist[0, :, 0] -= sa
 load_hist[1, :, 0] += sa
+print load_hist
+print evaluate_findley(combined_stress=load_hist, a_cp=np.array([k]), worker_run_out_time=8000,
+                       num_workers=8, chunk_size=300, search_grid=10)
 
+sa = 760.
+
+load_hist = np.zeros((2, 1, 6))
+load_hist[:, :, 0] = -250
+load_hist[:, :, 1] = -250
+load_hist[0, :, 0] -= sa
+load_hist[1, :, 0] += sa
+load_hist[0, :, 1] -= sa
+load_hist[1, :, 1] += sa
 print load_hist
 
 print evaluate_findley(combined_stress=load_hist, a_cp=np.array([k]), worker_run_out_time=8000,
                        num_workers=8, chunk_size=300, search_grid=10)
-
