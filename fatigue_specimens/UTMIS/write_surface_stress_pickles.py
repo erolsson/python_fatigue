4aa51691ff5cda1ef6f119ad842543d235906573
erolsson@kth.se <erolsson@kth.se>
2019-05-21 16:33:02 +0200

    implementing Drucker-prager for 2506

--- fatigue_specimens/UTMIS/write_surface_stress_pickles.py
+++ fatigue_specimens/UTMIS/write_surface_stress_pickles.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
                       'notched': {-1.: [427., 450.], 0.: [225., 240., 255.]}}
     simulations = []
     reader = InputFileReader()
-    positions = None
+    positions = {'smooth': None, 'notched': None}
     for specimen, data in specimen_loads.iteritems():
         for R, stress_levels in data.iteritems():
             simulations += [FEMSimulation(specimen=specimen, stress=s, R=R) for s in stress_levels]
@@ -36,19 +36,20 @@ if __name__ == '__main__':
                                                          get_position_numbers=True)
             n = stress.shape[0]
             stress_data[level] = np.zeros((2*n, 6))
-            if positions is None:
+            if positions[simulation.specimen] is None:
                 geom_file = 'utmis_' + simulation.specimen + '/utmis_' + simulation.specimen + '.inc'
                 reader.read_input_file(geom_file)
                 nodal_coords = reader.nodal_data
-                positions = np.zeros((2*n, 3))
+                pos = np.zeros((2*n, 3))
                 for i, label in enumerate(node_labels):
-                    positions[i, :] = nodal_coords[label - 1, 1:]
-                positions[n:, :] = positions[:n, :]
-                positions[n:, 1] *= -1
+                    pos[i, :] = nodal_coords[label - 1, 1:]
+                pos[n:, :] = pos[:n, :]
+                pos[n:, 1] *= -1
+                positions[simulation.specimen] = pos
             stress_data[level][:n, :] = stress
             stress_data[level][n:, :] = read_field_from_odb('S', odb_name, step_name, frame_number=-1,
                                                             element_set_name='EXPOSED_ELEMENTS',
                                                             instance_name='specimen_part_neg'.upper())
         with open(pickle_path + '_surface_stresses_' + name, 'w') as pickle_handle:
             pickle.dump(stress_data, pickle_handle)
-            pickle.dump(positions, pickle_handle)
+            pickle.dump(positions[simulation.specimen], pickle_handle)
