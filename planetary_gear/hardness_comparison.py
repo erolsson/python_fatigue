2fab5eae41ad58fe8ef229c46bccebbf97c38039
erolsson@kth.se <erolsson@kth.se>
2020-11-18 19:48:53 +0100

    "writing stresses from utmis specimens"

--- planetary_gear/hardness_comparison.py
+++ planetary_gear/hardness_comparison.py
@@ -28,11 +28,9 @@ def main():
                    Simulation(cd=1.1, color='g', sym='d'), Simulation(cd=1.4, color='k', sym='p')]
 
     experimental_hardness = np.genfromtxt(experiment_path + 'hardness/hardness_scania.csv')
-
-    carbon_levels = np.array([0.1, 0.2, 0.4, 0.6, 0.8, 1.0])/100
-    t_mart_old = np.array([38., 45., 52., 59., 71.5, 72.])
-    t_mart_new = np.array([38., 52., 54., 61., 67.5, 70.])
-    austenite_old = np.array([21., 23., 26., 29., 32., 35.])
+    experimental_carbon = np.genfromtxt(experiment_path + 'carbon_profiles/carbon_profiles.csv',
+                                        delimiter='\t')
+    print(experimental_carbon)
 
     for i, simulation in enumerate(simulations):
         plt.figure(0)
@@ -54,33 +52,8 @@ def main():
         plt.figure(2)
         plt.plot(root_data['r'], root_data['SDV_CARBON'], '--' + simulation.color, lw=2)
 
-        plt.figure(3)
-        plt.plot(root_data['r'], root_data['SDV_AUSTENITE'], '--' + simulation.color, lw=2)
-
-        plt.figure(4)
-        plt.plot(root_data['r'], flank_data['SDV_UBAINITE'] + flank_data['SDV_LBAINITE'], '--' + simulation.color, lw=2)
-
-        plt.figure(5)
-        plt.plot(flank_data['r'], flank_data['SDV_T_MARTENSITE'], '--' + simulation.color, lw=2)
-        plt.plot(root_data['r'], root_data['SDV_T_MARTENSITE'], '--' + simulation.color, lw=2)
-
-        interp_exp = np.interp(root_data['r'], experimental_hardness[:, 0], experimental_hardness[:, 2*i + 4])
-        plt.figure(1)
-        plt.plot(root_data['r'], interp_exp, ':' + simulation.color, lw=2)
-
-        plt.figure(6)
-        plt.plot(experimental_hardness[:, 0], HV2HRC(experimental_hardness[:, 2*i + 4]), simulation.color, lw=2)
-        plt.plot(root_data['r'], HV2HRC(root_data['HV']), '--' + simulation.color, lw=2)
-
-        hv_mart_old = HRC2HV(interp1d(carbon_levels, t_mart_old)(root_data['SDV_CARBON'])[:, 0])
-        hv_mart_new = HRC2HV(interp1d(carbon_levels, t_mart_new)(root_data['SDV_CARBON'])[:, 0])
-        hv_austenite = HRC2HV(interp1d(carbon_levels, austenite_old)(root_data['SDV_CARBON'])[:, 0])
-        hv_mart_aust_old = (hv_mart_old*root_data['SDV_T_MARTENSITE'][:, 0]
-                            + hv_austenite*root_data['SDV_AUSTENITE'][:, 0])
-        hv_mart_aust_new = (hv_mart_new*root_data['SDV_T_MARTENSITE'][:, 0]
-                            + hv_austenite*root_data['SDV_AUSTENITE'][:, 0])
-        plt.figure(10)
-        plt.plot(root_data['r'], root_data['HV'][:, 0] - hv_mart_aust_old, '--' + simulation.color, lw=2)
+        plt.plot(experimental_carbon[:, 1], experimental_carbon[:, 3+i]/100, '-' + simulation.sym + simulation.color,
+                 lw=2, ms=12)
 
     fig = plt.figure(1)
     plt.plot([-1, -2], [0, 0], 'k', lw=2, label='Exp.')
