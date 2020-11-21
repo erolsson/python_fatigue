02a980de2165f26e65917da09c102ad7c71503ec
erolsson@kth.se <erolsson@kth.se>
2020-11-21 12:17:30 +0100

    "writing stresses from utmis specimens"

--- abaqus_files/write_heat_treatment_path_data.py
+++ abaqus_files/write_heat_treatment_path_data.py
@@ -73,7 +73,7 @@ def main():
     odb_file = os.path.expanduser('~/scania_gear_analysis/odb_files/heat_treatment/mesh_1x/'
                                   'dante_results_tempering_60min_200C_HRD_1.odb')
     pickle_path = os.path.expanduser('~/scania_gear_analysis/pickles/heat_treatment/mesh_1x/root_data/'
-                                     'tempering_60min_200C_HRD_1/')
+                                     'carbon_potential.odb/')
     if not os.path.isdir(pickle_path):
         os.makedirs(pickle_path)
     for cd in [0.5, 0.8, 1.1, 1.4]:
